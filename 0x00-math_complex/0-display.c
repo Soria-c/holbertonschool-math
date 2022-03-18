b832b935aca88f60bc1400980181514bb936376a
Carlos Soria <soriac367@gmail.com>
2022-03-18 12:11:14 -0500

    Function that prints a complex number

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -11,14 +11,30 @@ void display_complex_number(complex c)
 	if (c.im < 0)
 	{
 		if (!c.re)
-			printf("%.9gi\n", c.im);
+			if (-(c.im) == 1)
+				printf("-i\n");
+			else
+				printf("%gi\n", c.im);
 		else
-			printf("%.9g - %.9gi\n", c.re, -(c.im));
+			if (-(c.im) == 1)
+				printf("%g - i\n", c.re);
+			else
+				printf("%g - %gi\n", c.re, -(c.im));
 	}
 	else if (!c.im)
-		printf("%.9g\n", c.re);
+		printf("%g\n", c.re);
 	else if (!c.re)
-		printf("%.9gi\n", c.im);
+	{
+		if (c.im == 1)
+			printf("i\n");
+		else
+			printf("%gi\n", c.im);
+	}
 	else
-		printf("%.9g + %.9gi\n", c.re, c.im);
+	{
+		if (c.im == 1)	
+			printf("%g + i\n", c.re);
+		else
+			printf("%g + %gi\n", c.re, c.im);
+	}
 }
