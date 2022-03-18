a75b43374a2f406c39c1bb9cd67d720e875b868d
Carlos Soria <soriac367@gmail.com>
2022-03-18 12:54:06 -0500

    Function that substracts two complex numbers

---
+++ 0x00-math_complex/5-substraction.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * substraction - substracts two complex numbers
+ * @c1: input complex number.
+ * @c2: input complex number.
+ * @c3: address to store the result
+ */
+
+void substraction(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re - c2.re;
+	c3->im = c1.im - c2.im;
+}
