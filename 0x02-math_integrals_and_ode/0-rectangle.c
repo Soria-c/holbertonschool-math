e7cb14fdfaa5d0711116e8676daf1a346f511091
Carlos Soria <Soriac367@gmail.com>
2022-04-15 21:49:35 -0400

    Fixed style

--- 0x02-math_integrals_and_ode/0-rectangle.c
+++ 0x02-math_integrals_and_ode/0-rectangle.c
@@ -1,21 +1,20 @@
 #include "rectangle.h"
 /**
  * rectangle_method - computes an integreal, rectangle method.
- * a: lower bound.
- * b: upper bound.
- * steps: number of rectangles.
+ * @a: lower bound.
+ * @b: upper bound.
+ * @steps: number of rectangles.
  * Return: integral.
  */
-double rectangle_method(double a, double b, int steps )
+double rectangle_method(double a, double b, int steps)
 {
-    double width = (b - a) / steps;
-    double area = 0;
-    double x = 0;
-    int i;
+	double width = (b - a) / steps;
+	double area = x = 0;
+	int i;
 
-    for (i = 0; i < steps; i++, x += width)
-        area += height(x) * width;
-    return (area);
+	for (i = 0; i < steps; i++, x += width)
+		area += height(x) * width;
+	return (area);
 }
 
 /**
@@ -25,5 +24,5 @@ double rectangle_method(double a, double b, int steps )
  */
 double height(double x)
 {
-    return (1 / (1 + pow(x, 2)));
+	return (1 / (1 + pow(x, 2)));
 }
--- 0x02-math_integrals_and_ode/rectangle.h
+++ 0x02-math_integrals_and_ode/rectangle.h
@@ -3,8 +3,7 @@
 
 #include <math.h>
 
-double rectangle_method(double a, double b, int steps );
+double rectangle_method(double a, double b, int steps);
 double height(double x);
-
 
 #endif /*RECTANGLE_H*/
