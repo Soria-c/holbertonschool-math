e881b8bf0943a5e2bbeee6ed6478c25df4ab5492
Carlos Soria <soriac367@gmail.com>
2022-04-30 15:26:05 -0500

    Gaussian elimination in python

--- 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
+++ 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
@@ -5,17 +5,16 @@ import numpy as np
 def gaussian_elimination(A, b):
     b = np.expand_dims(b, axis=0)
     c = np.concatenate((A, b.T), axis=1)
+    if not(np.linalg.det(A)):
+        print("You can't divide by zero!")
+        return
     lenght = len(c)
     l2 = len(c[0])
     for i in range(0, lenght - 1):
         for j in range(i + 1, lenght):
             c[j] = np.array(c[i] * ((c[j][i] / c[i][i]) * -1)) + c[j]
     r = np.zeros(shape=lenght)
-    if not(c[lenght - 1][l2 - 2]):
-        print("You can't divide by zero!")
-        return
-    else:
-        x = c[lenght - 1][l2 - 1] / c[lenght - 1][l2 - 2]
+    x = c[lenght - 1][l2 - 1] / c[lenght - 1][l2 - 2]
     r[lenght - 1] = x
     for i in range(lenght - 2, -1, -1):
         summ = 0
